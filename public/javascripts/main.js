33cfafdac9c7bdee68a3cf827e2ef731ab3c0471
qdecrouy <quentin.decrouy@gmail.com>
2016-08-30 14:26:13 +0200

    [QDY] [30/08/2016] : Commencement boutons Save et Modify

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,25 @@
 $( document ).ready(function() {
+    // Click Modifier
+    $('.modifyBtn').click(function(){
+        if ($(this).parent()[0] == $($('.table tbody tr')[7]).children()[7]){
+            alert('Modification 1 !');
+        }
+        else {
+            alert('Modification 2 !');
+        }
+    });
+
+    // Click Enregistrer
+    $('.saveBtn').click(function(){
+        if ($(this).parent()[0] == $($('.table tbody tr')[7]).children()[7]){
+            alert('Enregistrement 1 !');
+        }
+        else {
+            alert('Enregistrement 2 !');
+        }
+    });
+
+    // Click Refresh
     $('#refreshBtn').click(function(){
         var that = this;
         that.disabled = true;
